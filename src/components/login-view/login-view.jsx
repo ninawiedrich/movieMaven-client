e99fcf839ae8658a414f36e089a1b96b204cdaee
Nina Wiedrich <94755570+ninawiedrich@users.noreply.github.com>
2023-07-30 22:54:34 +0200

    Added login and signup views with form validation, persistent sessions, and integrated with MainView

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+export const LoginView = ({onLoggedIn}) => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    // prevents the default refresh/change of page
+    event.preventDefault();
+
+    const data = {
+      username: username,
+      password: password
+    };
+
+    fetch("https://moviemaven-dfc40ecb1c33.herokuapp.com/login", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+      }).then((response) => response.json())
+      .then((data) => {
+      console.log("Login response:", data);
+      if (data.user) {
+      onLoggedIn(data.user, data.token);
+      } else { 
+      alert ("No such user.");
+      }
+  })
+  .catch((e) => {
+    alert("Something went wrong.");
+  });
+};
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
